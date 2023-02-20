ba5a055ea3cb43d182a311f86a98fe72f6c65331
jurakin <jurakin.dev@gmail.com>
2023-02-20 11:30:55 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from src.parallel_downloader import __version__
+
+setup(
+    name="parallel-downloader",
+    description="Downloads file using parallel downloading.",
+    version=__version__,
+    license="GNU General Public License v3.0",
+    author_email="jurakin.dev@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/jurakin/parallel-downloader",
+    keywords="parallel downloader",
+    python_requires=">=3.7",
+    install_requires=[
+          "requests",
+          "argparse"
+    ],
+    entry_points={
+        "console_scripts": [
+            "parallel-downloader = src.parallel_downloader.main:run",
+        ]
+    }
+)
---
+++ src/parallel_downloader/__init__.py
@@ -0,0 +1,9 @@
+from .downloader import ParallelDownloader
+from .downloader import ParallelDownloaderError
+
+__all__ = [
+    "ParallelDownloader",
+    "ParallelDownloaderError",
+]
+
+__version__ = "1.0"
